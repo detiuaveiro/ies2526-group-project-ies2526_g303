2308d2295fc0f6cdaf40e935d207e01baf860588
Rodrigo Pinho da Fonseca <rodrigo.pinho.fonseca@ua.pt>
2026-04-15 22:46:35 +0000

    Inicializacao do Maven com o Docker

--- backend/README.md
+++
@@ -1,9 +0,0 @@
-# Backend
-
-Este subprojeto será responsável pelo backend da aplicação:
-
-- Receber os dados do simulador
-- Processar e armazenar no banco de dados
-- Expor a API REST para o frontend
-
-> Atualmente apenas criado para manter a estrutura do repositório. Implementação futura.
--- backend/src/main/java/smarthome/backend/BackendApplication.java
+++ backend/src/main/java/smarthome/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package pt.ua.deti.ies.g303.backend;
+package smarthome.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmarthomeBackendApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmarthomeBackendApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
